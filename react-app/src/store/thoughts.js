bc44201bd704f3a887638fb6ead0a96b1149cea8
IsabelArredondo <izzyaround@gmail.com>
2022-10-25 16:11:36 -0500

    thoughts.js line 84

--- react-app/src/store/thoughts.js
+++ react-app/src/store/thoughts.js
@@ -88,7 +88,7 @@ export const createThought = (formData) => async (dispatch) => {
     });
 
     if (response.ok) {
-    const newThought = await response.json();
+    const newThought = await response;
     dispatch(createThoughts(newThought));
     return newThought
     } 
